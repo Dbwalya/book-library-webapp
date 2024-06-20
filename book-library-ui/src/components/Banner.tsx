394c2addcd9e9c183cc60d5206580c782b2d34cc
dbwalya <danielbwalya620@gmail.com>
2024-06-21 00:25:30 +0200

    Banner text updated

--- book-library-ui/src/components/Banner.tsx
+++ book-library-ui/src/components/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="flex py-4">
       <div className="flex flex-col items-center bg-gray-200 mx-auto md:w-4/5 py-10 px-8 rounded-2xl">
         <h2 className="mb-3 font-bold text-4xl">Build Your Library</h2>
-        <h3 className="text-gray-800">Buy two selected books and get <br /> one for free</h3>
+        <h3 className="text-gray-800">Organize and manage your book collection with ease.</h3>
 
         <AddBookModal />
       </div>
